9fbd158b55cdcec6ee75af1e70b1f96181d98339
ammarib <ibmammar@gmail.com>
2020-12-28 22:44:25 +0200

    paragraph background color and font size

--- lab: 02/script.js
+++ lab: 02/script.js
@@ -1,10 +1,16 @@
+'use strict'
+
+
 var name = prompt("Hello! what is your name ?")
 alert("welcome to my page " + name);
 console.log(name)
-var ansewr = prompt("are you a LTUC student ? yes/no")
-if (ansewr.toLocaleLowerCase() === 'yes') {
-    var LTUCstudent = prompt("are you software engineeer student? yes/no")
 
-}else if (ansewr.toLocaleLowerCase() === 'no'){
-    var notLTUCstudent = prompt("do have any programing expeirence ? yes/no")
+var ansewr = prompt("are you a LTUC student ? yes/no")
+
+if (ansewr.toLocaleLowerCase() === 'yes' || ansewr.toLocaleLowerCase() === 'y') {
+     prompt("are you software engineeer student? yes/no")
+    
+} else if (ansewr.toLocaleLowerCase() === 'no'|| ansewr.toLocaleLowerCase() ==='n' ) {
+      prompt("do have any programing expeirence ? yes/no")
+    
 }
