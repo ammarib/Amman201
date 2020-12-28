f4e3487f9c532e166e8058cac1289ee9b4da9bdb
ammarib <ibmammar@gmail.com>
2020-12-28 23:31:15 +0200

    java script name quistion and null answer

--- lab: 02/script.js
+++ lab: 02/script.js
@@ -1,16 +1,24 @@
 'use strict'
 
 
-var name = prompt("Hello! what is your name ?")
-alert("welcome to my page " + name);
-console.log(name)
+var thename = prompt("Hello! what is your name ?")
+
+
+ if(thename == null) {
+    alert("Hello!Welcome to my page my guest!")
+}
+else {
+    alert("welcome to my page " + thename) 
+}
+
+console.log(thename)
 
 var ansewr = prompt("are you a LTUC student ? yes/no")
 
 if (ansewr.toLocaleLowerCase() === 'yes' || ansewr.toLocaleLowerCase() === 'y') {
-     prompt("are you software engineeer student? yes/no")
-    
-} else if (ansewr.toLocaleLowerCase() === 'no'|| ansewr.toLocaleLowerCase() ==='n' ) {
-      prompt("do have any programing expeirence ? yes/no")
-    
+    prompt("are you software engineeer student? yes/no")
+
+} else if (ansewr.toLocaleLowerCase() === 'no' || ansewr.toLocaleLowerCase() === 'n') {
+    prompt("do have any programing expeirence ? yes/no")
+
 }
