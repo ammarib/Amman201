f0a4c7f7b401441946c3b00f0dcb2248d4829053
ammarib <ibmammar@gmail.com>
2020-12-28 23:49:54 +0200

    adding services offer var

--- lab: 02/script.js
+++ lab: 02/script.js
@@ -4,11 +4,11 @@
 var thename = prompt("Hello! what is your name ?")
 
 
- if(thename == null) {
+if (thename == null) {
     alert("Hello!Welcome to my page my guest!")
 }
 else {
-    alert("welcome to my page " + thename) 
+    alert("welcome to my page " + thename)
 }
 
 console.log(thename)
@@ -20,4 +20,17 @@ if (aboutguest.toLocaleLowerCase() === 'yes' || aboutguest.toLocaleLowerCase() =
 
 } else if (aboutguest.toLocaleLowerCase() === 'no' || aboutguest.toLocaleLowerCase() === 'n') {
     prompt("do have any programing expeirence ? yes/no")
+}
 
+console.log(aboutguest)
+
+var serviceoffer = prompt(" would you like to get some technical help ? yes/no") 
+if(serviceoffer.toLocaleLowerCase() === 'yes' || serviceoffer.toLocaleLowerCase() === 'y'){
+    alert("Contact me via Gmail botton link in my page")
+} 
+else if(serviceoffer.toLocaleLowerCase() === 'no' || serviceoffer.toLocaleLowerCase() === 'n'){
+    alert("Thank for your patience :) and welcome again " + thename )
+}
+else {
+    alert ("welcome to my page "+ thename)
+}
